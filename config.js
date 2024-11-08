9f0df61cfc1d8e9162e1cb6322bdc47123bde44b
KyleWalther <kylerickwalther@yahoo.com>
2024-11-07 21:29:52 -0600

    adding files again to fix env again

--- config.js
+++ config.js
@@ -1,24 +1,17 @@
 "use strict";
 
-/** Shared config for application; can be required many places. */
-
 require("dotenv").config();
 require("colors");
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret-dev";
-
 const PORT = +process.env.PORT || 3001;
 
-// Use dev database, testing database, or via env var, production database
+// Function to get the database URI based on the environment
 function getDatabaseUri() {
-  const user = "kylewalther"; // replace with your actual username
-  const password = "password"; // replace with your actual password
-  const dbName = process.env.NODE_ENV === "test" ? "jobly_test" : "jobly";
-  
-  return `postgresql://${user}:${password}@localhost:5432/${dbName}`;
+  return process.env.DATABASE_URL || "postgresql://default_user:default_pass@localhost:5432/jobly";
 }
 
-// Speed up bcrypt during tests, since the algorithm safety isn't being tested
+// Speed up bcrypt during tests
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
 console.log("Jobly Config:".green);
