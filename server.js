75b7e9363a67f93125799ce304350aa8237586e4
KyleWalther <kylerickwalther@yahoo.com>
2024-11-07 21:47:25 -0600

    adding cors to server file again

--- server.js
+++ server.js
@@ -4,12 +4,24 @@ const app = require("./app");
 const { PORT } = require("./config");
 const cors = require("cors");  // Import the CORS middleware
 
-// Configure CORS to allow your frontend's origin
 const corsOptions = {
-  origin: "https://front-end-jobly.onrender.com",  // Allow this origin
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:3000",    // Local dev
+      "https://front-end-jobly.onrender.com",  // Production (Render)
+    ];
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
 };
+
+app.use(cors(corsOptions));
 
 // Use CORS middleware with your options
 app.use(cors(corsOptions));
